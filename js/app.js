83788e0ca8c7d6aaa41f924c659f21499d595bf8
Howard Huang <hh83917@mac.com>
2015-11-09 21:58:38 -0600

    Completed nbaRoutes Project

--- js/app.js
+++ js/app.js
@@ -5,5 +5,21 @@ app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
     $httpProvider.interceptors.push('httpRequestInterceptor');
 
     // routing configuration code
-
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'js/home/homeTmpl.html',
+        controller: 'homeCtrl'
+      })
+      .state('teams', {
+        url: '/teams/:team',
+        templateUrl: 'js/teams/teamTmpl.html',
+        controller: 'teamCtrl',
+        resolve: {
+          teamData: function(teamService, $stateParams) {
+            return teamService.getTeamData($stateParams.team);
+          }
+        }
+      });
+    $urlRouterProvider.otherwise('/');
 });
--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,35 @@
 var app = angular.module('nbaRoutes');
 // the resolved data from the router needs to be injected into the controller
-app.controller('teamCtrl', function ($scope, $stateParams, teamService) {
+app.controller('teamCtrl', function ($scope, $stateParams, teamService, teamData) {
+  $scope.teamData = teamData;
+  $scope.newGame = {};
+  $scope.showNewGameForm = false;
+  $scope.toggleNewGameForm = function() {
+      $scope.showNewGameForm = !$scope.showNewGameForm;
+  };
 
+  if($stateParams.team === "utahjazz") {
+    $scope.homeTeam = "Utah Jazz";
+    $scope.logoPath = 'images/jazz-logo.png';
+  }
+  if($stateParams.team === "losangeleslakers") {
+    $scope.homeTeam = "Los Angeles Lakers";
+    $scope.logoPath = 'images/lakers-logo.png';
+  }
+  if($stateParams.team === "miamiheat") {
+    $scope.homeTeam = "Miami Heat";
+    $scope.logoPath = 'images/heat-logo.png';
+  }
+  $scope.submitGame = function() {
+    $scope.newGame.homeTeam = $scope.homeTeam.split(' ').join('').toLowerCase();
+    teamService.addNewGame($scope.newGame)
+    .then(function() {
+      teamService.getTeamData($scope.newGame.homeTeam)
+      .then(function(ret) {
+        $scope.teamData = ret;
+        $scope.newGame = {};
+        $scope.showNewGameForm = false;
+      });
+    });
+  };
 });
